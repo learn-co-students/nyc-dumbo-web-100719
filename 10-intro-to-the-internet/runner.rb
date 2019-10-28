183645feed6d68d2be793e7b00449fa615a31d7e
Ian Hollander <ianholla@gmail.com>
2019-10-28 13:40:07 -0400

    before intro to sinatra

--- 10-intro-to-the-internet/runner.rb
+++ 10-intro-to-the-internet/runner.rb
@@ -1,13 +1,29 @@
 require 'sinatra'
 
+# requests
 get '/' do
   # respond
   "<h1>sup</h1>"
 end
 
+# requests
 get '/sup' do
   "Hello World"
 end
+
+# requests
+get '/search' do
+  # look something up in the database
+  # format some HTML page 
+  # send that as the response
+end
+
+# requests
+post '/kittens' do
+  # add a kitten to the database
+  # send some response
+end
+
 
 # if http_verb == "GET" && path == "/"
 #   return "<h1>sup</h1>"
