4412a88c23ef4756e3efc19f9d33ee81e6016a94
Eric Kim <hyeokjungkim@gmail.com>
2019-11-22 09:27:27 -0500

    adds lecture

---
+++ 30-toy-tales-review/src/index.js
@@ -0,0 +1,16 @@
+let addToy = false
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const addBtn = document.querySelector('#new-toy-btn')
+  const toyForm = document.querySelector('.container')
+  addBtn.addEventListener('click', () => {
+    // hide & seek with the form
+    addToy = !addToy
+    if (addToy) {
+      toyForm.style.display = 'block'
+    } else {
+      toyForm.style.display = 'none'
+    }
+  })
+
+})
