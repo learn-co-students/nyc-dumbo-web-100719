208315b6987048a62e39bd131d7ecb3077bed8c7
Eric Kim <hyeokjungkim@gmail.com>
2019-12-02 14:26:21 -0500

    adds lecture

---
+++ 33-oojs/index.js
@@ -0,0 +1,55 @@
+const nicky = {
+  fullName: "Nicky Dover",
+  name: "Nicky",
+  introduce: function(){
+    return "Hi, I'm Nicky Dover."
+  }
+}
+
+const annie = {
+  fullName: "Annie Souza",
+  name: "Annie",
+  introduce: function(){
+    return "Hi, I'm Annie Souza."
+  }
+}
+
+const eric = {
+  legalName: "Hyeok Jung Kim",
+  name: "Eric",
+  introduce: function(){
+    return "Hi, I'm Hyeok Jung Kim."
+  }
+}
+
+console.log(nicky.introduce())
+console.log(eric.introduce())
+console.log(annie.introduce())
+
+// console.log(eric.introduce() === nicky.introduce())
+// console.log(eric.introduce === nicky.introduce)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
