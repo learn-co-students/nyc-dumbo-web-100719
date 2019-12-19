26e1eda8d5c819c28e3da36bc87249a0e11cfa18
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-12-19 11:00:51 -0500

    starting point

--- 42-react-router/src/App.js
+++ 42-react-router/src/App.js
@@ -3,9 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import NotFound from './NotFound'
 import Home from './Home'
-import { SpiceGirl /*, Scary, Baby, Ginger, Posh, Sporty*/  } from './spice-girls';
-import { Route, Switch, Link, NavLink } from 'react-router-dom'
-
+import { SpiceGirl, Scary, Baby, Ginger, Posh, Sporty } from './spice-girls';
 
 export default class App extends React.Component {
 
@@ -18,82 +16,45 @@ export default class App extends React.Component {
           <aside className="sidebar">
             <ul>
               <li>
-                <NavLink exact  to="/">Home</NavLink>
+                <a href="/">Home</a>
               </li>
-
-              {
-                this.state.spiceGirls.map(spice => {
-                  return <li>
-                    <NavLink
-                             to={ "/spice/" + spice.slug }>
-                    { spice.adjective }
-                    </NavLink>
-                  </li>
-                })
-              }
-              
             </ul>
           </aside>
         </header>
-
-        <Switch>
-          {
-          // <Route path="/geri" component={ Ginger } />
-          // <Route path="/mel-b" component={ Scary } />
-          // <Route path="/mel-c" component={ Sporty } />
-          // <Route path="/victoria" component={ Posh } />
-          }
-          <Route path="/spice/:slug" render={ this.renderSpiceGirl } />
-          <Route exact path="/" component={ Home } />
-          <Route component={ NotFound } />
-        </Switch>
-
-
-        { 
-          //<iframe width="560" height="315" src="https://www.youtube.com/embed/P8WCVZ2kHI8?start=24&autoplay=true" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        }
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/P8WCVZ2kHI8?start=24&autoplay=true" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     );
   }
 
-  renderSpiceGirl = (renderParams) => {
-    // console.log(renderParams)
-    const slug = renderParams.match.params.slug
-    const spiceGirl = this.state.spiceGirls.find(spice => spice.slug === slug)
-    if (spiceGirl)
-      return <SpiceGirl adjective={ spiceGirl.adjective } gif={ spiceGirl.gif }/>
-    else return <NotFound />
-  }
-
-  state = {
-    spiceGirls: [
-      {
-        id: 1,
-        adjective: "Scary",
-        gif: "https://media.giphy.com/media/Eaa3LF8anrRm/giphy.gif",
-        slug: "mel-b",
-      },
-      {
-        id: 2,
-        adjective: "Baby",
-        gif: "https://media.giphy.com/media/PSfMwrLPXUtrO/giphy.gif",
-        slug: "emma",
-      },
+  // state = {
+  //   spiceGirls: [
+  //     {
+  //       id: 1,
+  //       adjective: "Scary",
+  //       gif: "https://media.giphy.com/media/Eaa3LF8anrRm/giphy.gif",
+  //       slug: "mel-b",
+  //     },
+  //     {
+  //       id: 2,
+  //       adjective: "Baby",
+  //       gif: "https://media.giphy.com/media/PSfMwrLPXUtrO/giphy.gif",
+  //       slug: "emma",
+  //     },
       
-      {
-        id: 4,
-        adjective: "Posh",
-        gif: "https://media.giphy.com/media/f4X5yhZlnZXLa/giphy.gif",
-        slug: "victoria",
-      },
-      {
-        id: 5,
-        adjective: "Sporty",
-        gif: "https://media.giphy.com/media/ag5PDZoSMrFuM/giphy.gif",
-        slug: "mel-c",
-      },
-    ]
-  }
+  //     {
+  //       id: 4,
+  //       adjective: "Posh",
+  //       gif: "https://media.giphy.com/media/f4X5yhZlnZXLa/giphy.gif",
+  //       slug: "victoria",
+  //     },
+  //     {
+  //       id: 5,
+  //       adjective: "Sporty",
+  //       gif: "https://media.giphy.com/media/ag5PDZoSMrFuM/giphy.gif",
+  //       slug: "mel-c",
+  //     },
+  //   ]
+  // }
 
 }
 
--- 42-react-router/src/index.js
+++ 42-react-router/src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
