a46b33af0d2435473356c9cb3d6781d7e2df0f96
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-12-31 10:33:54 -0500

    router

--- 42-react-router/src/App.js
+++ 42-react-router/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import NotFound from './NotFound'
 import Home from './Home'
+import { Route, Switch } from 'react-router'
+import { NavLink } from 'react-router-dom'
 import { SpiceGirl, Scary, Baby, Ginger, Posh, Sporty } from './spice-girls';
 
 export default class App extends React.Component {
@@ -16,45 +18,75 @@ export default class App extends React.Component {
           <aside className="sidebar">
             <ul>
               <li>
-                <a href="/">Home</a>
+                <NavLink to="/" exact activeClassName="selected">Home</NavLink>
               </li>
+              {
+                this.state.spiceGirls.map(spice => <li>
+                <NavLink to={ "/spice/" + spice.slug } activeClassName="selected">{ spice.adjective }</NavLink>
+              </li>)
+              }
             </ul>
           </aside>
         </header>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/P8WCVZ2kHI8?start=24&autoplay=true" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <Switch>
+          <Route path="/" exact component={ Home } />
+          { /*
+          <Route path="/mel-b" component={ Scary } />
+          <Route path="/geri" component={ Ginger } /> }
+          <Route path="/mel-c" component={ Sporty } />
+          <Route path="/victoria" render={ this.renderPosh } />
+          <Route path="/emma" component={ Baby } /> */}
+          <Route path="/spice/:slug" render={ this.renderSpice } />
+          <Route component={ NotFound } />
+        </Switch>
+
       </div>
     );
   }
 
-  // state = {
-  //   spiceGirls: [
-  //     {
-  //       id: 1,
-  //       adjective: "Scary",
-  //       gif: "https://media.giphy.com/media/Eaa3LF8anrRm/giphy.gif",
-  //       slug: "mel-b",
-  //     },
-  //     {
-  //       id: 2,
-  //       adjective: "Baby",
-  //       gif: "https://media.giphy.com/media/PSfMwrLPXUtrO/giphy.gif",
-  //       slug: "emma",
-  //     },
-      
-  //     {
-  //       id: 4,
-  //       adjective: "Posh",
-  //       gif: "https://media.giphy.com/media/f4X5yhZlnZXLa/giphy.gif",
-  //       slug: "victoria",
-  //     },
-  //     {
-  //       id: 5,
-  //       adjective: "Sporty",
-  //       gif: "https://media.giphy.com/media/ag5PDZoSMrFuM/giphy.gif",
-  //       slug: "mel-c",
-  //     },
-  //   ]
-  // }
+  renderSpice = renderProps => {
+    const slug = renderProps.match.params.slug
+    const spice = this.state.spiceGirls.find(spiceGirl => spiceGirl.slug === slug)
+    if (!spice) {
+      return <NotFound />
+    }
+    return <SpiceGirl adjective={ spice.adjective } gif={ spice.gif } />
+  }
+
+  state = {
+    spiceGirls: [
+      {
+        id: 1,
+        adjective: "Scary",
+        gif: "https://media.giphy.com/media/Eaa3LF8anrRm/giphy.gif",
+        slug: "mel-b",
+      },
+      {
+        id: 2,
+        adjective: "Baby",
+        gif: "https://media.giphy.com/media/PSfMwrLPXUtrO/giphy.gif",
+        slug: "emma",
+      },
+      {
+        id: 3,
+        adjective: "Ginger",
+        gif: "https://media.giphy.com/media/ZA1X3mZigRMoo/giphy.gif",
+        slug: "geri"
+      },
+      {
+        id: 4,
+        adjective: "Posh",
+        gif: "https://media.giphy.com/media/f4X5yhZlnZXLa/giphy.gif",
+        slug: "victoria",
+      },
+      {
+        id: 5,
+        adjective: "Sporty",
+        gif: "https://media.giphy.com/media/ag5PDZoSMrFuM/giphy.gif",
+        slug: "mel-c",
+      },
+    ]
+  }
 
 }
 
